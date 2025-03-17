6c836308bcf49c38bacea53ca2d9e35d8c8fe40a
donghyun <reg0212@naver.com>
2025-03-17 20:47:33 +0900

    [DOCS] 어노테이션 추가

--- src/main/java/com/hyun/twitter/follow/entity/Follow.java
+++ src/main/java/com/hyun/twitter/follow/entity/Follow.java
@@ -1,10 +1,16 @@
 package com.hyun.twitter.follow.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Follow {
 
     private Long followId;
