4414efedc6f3c9e93ed4a29fb9ea089f1fdb3fdb
donghyun <reg0212@naver.com>
2025-04-02 20:01:28 +0900

    [DOCS] 어노테이션 제거

--- src/main/java/com/hyun/twitter/postLike/mapper/PostLikeMapper.java
+++ src/main/java/com/hyun/twitter/postLike/mapper/PostLikeMapper.java
@@ -1,6 +1,5 @@
 package com.hyun.twitter.postLike.mapper;
 
-import com.hyun.twitter.follow.entity.Follow;
 import com.hyun.twitter.postLike.entity.PostLike;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
