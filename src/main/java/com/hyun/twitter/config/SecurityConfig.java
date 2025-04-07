2b59eb3121d3ac8d6a1eff8f089414d420d4cef9
donghyun <reg0212@naver.com>
2025-04-07 22:15:23 +0900

    [FEAT] security 설정

--- src/main/java/com/hyun/twitter/config/SecurityConfig.java
+++ src/main/java/com/hyun/twitter/config/SecurityConfig.java
@@ -2,13 +2,30 @@ package com.hyun.twitter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll() // 로그인, 회원가입 허용
+                        .anyRequest().authenticated() // 나머지는 인증 필요
+                );
+
+        return http.build();
     }
 }
