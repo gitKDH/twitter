f1379995f0a8fc2ace144f4be33fa1b4953dadaa
donghyun <reg0212@naver.com>
2025-04-16 21:34:04 +0900

    [DOCS] UserDetailsService를 위한 Impl 및 Custom 추가

---
+++ src/main/java/com/hyun/twitter/user/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.hyun.twitter.user.service;
+
+import com.hyun.twitter.user.entity.User;
+import com.hyun.twitter.user.mapper.UserMapper;
+import com.hyun.twitter.user.service.impl.UserDetailsImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserMapper userMapper;
+
+    public CustomUserDetailsService(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userMapper.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
+        }
+
+        return new UserDetailsImpl(user);
+    }
+}
