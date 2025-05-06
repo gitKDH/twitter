98d0b33dc84dd81e4933b859b1700761e7519918
donghyun <reg0212@naver.com>
2025-05-06 21:21:56 +0900

    [FEAT] FilterChain 직접등록

--- src/main/java/com/hyun/twitter/config/SecurityConfig.java
+++ src/main/java/com/hyun/twitter/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.hyun.twitter.config;
 
+import com.hyun.twitter.user.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -21,9 +29,18 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
 
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안 함
+                )
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll() // 모두 허용 임시
                         .anyRequest().authenticated() // 나머지는 인증 필요
+                )
+
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtUtil, userDetailsService),
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
