6eb7397ed135f695e72591d93f6876daf95e2efe
donghyun <reg0212@naver.com>
2025-05-26 21:00:30 +0900

    [DOCS] 프로필 수정을 위한 DTO 추가

---
+++ src/main/java/com/hyun/twitter/user/dto/UserProfileUpdateRequestDto.java
@@ -0,0 +1,14 @@
+package com.hyun.twitter.user.dto;
+
+public class UserProfileUpdateRequestDto {
+    private String username;
+    private String bio;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+}
