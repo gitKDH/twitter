8fc115cea80e215b3160d464b10cf32d64833378
donghyun <reg0212@naver.com>
2025-06-13 19:32:13 +0900

    [DATA] 어노테이션 추가

--- src/main/java/com/hyun/twitter/user/controller/UserController.java
+++ src/main/java/com/hyun/twitter/user/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
