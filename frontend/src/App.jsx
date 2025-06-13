58db5a7885dffd9f572096c8686c0cd3ef2aa22d
donghyun <reg0212@naver.com>
2025-06-13 18:32:16 +0900

    [DATA] App.jsx 각 페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import PostDetail from "./pages/PostDetail";
 
-// 임시 컴포넌트 (테스트용으로 먼저 생성해둘 거야)
 const HomePage = () => <h1>홈</h1>;
 const LoginPage = () => <h1>로그인</h1>;
 const SignupPage = () => <h1>회원가입</h1>;
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/post/:postId" element={<PostDetail />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
